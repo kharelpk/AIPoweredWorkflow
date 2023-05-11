99cb6e26eb932bce1aaa8edfd56f12fcad52c226
Prashanta Kharel <prash@prashantas-mbp.myfiosgateway.com>
2023-05-11 00:49:47 -0400

    fixed responsiveness issue

--- frontend/src/components/CodeDisplay.jsx
+++ frontend/src/components/CodeDisplay.jsx
@@ -1,40 +1,41 @@
 // import React from 'react';
-// import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
-// import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
-
-// const CodeDisplay = ({ code }) => {
-//   return (
-//     <div className="code-display">
-//       <SyntaxHighlighter language="python" style={dracula}>
-//         {code}
-//       </SyntaxHighlighter>
-//     </div>
-//   );
-// };
-
-// export default CodeDisplay;
-
-// import React from 'react';
 // import MonacoEditor from '@uiw/react-monacoeditor';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faCopy } from '@fortawesome/free-solid-svg-icons';
+// import './CodeDisplay.scss';
 
 // const CodeDisplay = ({ code }) => {
+//   const copyCodeToClipboard = () => {
+//     navigator.clipboard.writeText(code);
+//   };
+
 //   return (
 //     <div className="code-display">
-//       <MonacoEditor
-//         language="python"
-//         value={code}
-//         options={{
-//           fontSize: 14,
-//           theme: 'vs-dark',
-//           readOnly: true,
-//         }}
-//         height="100px"
-//       />
+//       <div className="code-display-header">
+//         <h6 className='text-[#26947a]'>Generated Code</h6>
+//         <button onClick={copyCodeToClipboard}>
+//           <FontAwesomeIcon icon={faCopy} /> Copy
+//         </button>
+//       </div>
+//       <div className="code-display-body">
+//         <MonacoEditor
+//           language="python"
+//           value={code}
+//           options={{
+//             fontSize: 14,
+//             theme: 'vs-dark',
+//             readOnly: false,
+//           }}
+//           height="55vh" 
+//           width = "100%"
+//         />
+//       </div>
 //     </div>
 //   );
 // };
 
 // export default CodeDisplay;
+
 
 import React from 'react';
 import MonacoEditor from '@uiw/react-monacoeditor';
@@ -50,23 +51,25 @@ const CodeDisplay = ({ code }) => {
   return (
     <div className="code-display">
       <div className="code-display-header">
-        <h6 className='text-[#26947a]'>Generated Code</h6>
+        <h6 className='text-[#26947A]'>GENERATED CODE</h6>
         <button onClick={copyCodeToClipboard}>
           <FontAwesomeIcon icon={faCopy} /> Copy
         </button>
       </div>
       <div className="code-display-body">
-        <MonacoEditor
-          language="python"
-          value={code}
-          options={{
-            fontSize: 14,
-            theme: 'vs-dark',
-            readOnly: false,
-          }}
-          height="55vh"
-          width = "100%"
-        />
+      <div className="monaco-container">
+            <MonacoEditor
+            language="python"
+            value={code}
+            options={{
+                fontSize: 14,
+                theme: 'vs-dark',
+                readOnly: false,
+            }}
+            height="50vh"
+            width="100%"
+            />
+  </div>
       </div>
     </div>
   );
