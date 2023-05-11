668976800c3c73c3f5a2dc7a2bffed4476a89940
Prashanta Kharel <prash@prashantas-mbp.myfiosgateway.com>
2023-05-10 23:08:52 -0400

    added icons

--- frontend/src/components/DeviceCard.jsx
+++ frontend/src/components/DeviceCard.jsx
@@ -13,13 +13,36 @@
 
 // export default DeviceCard;
 
+// import React from 'react';
+
+// const DeviceCard = ({ title, description, info, image}) => {
+//   return (
+//     <div className="relative group">
+//       <div className="bg-[#202124] text-white w-54 h-30 p-2 m-2 rounded border border-[#26947a] cursor-pointer">
+//         <h3 className="text-lg font-bold">{title}</h3>
+//         <img src={image} alt="image" className="h-15 w-auto text-white fill-current" />
+//         <p className="text-xs mt-2 truncate group-hover:whitespace-normal group-hover:text-xs">
+//           {description}
+//         </p>
+//       </div>
+//       {/* <span className="hidden group-hover:block absolute z-10 top-full left-1/2 -translate-x-1/2 mt-2 px-3 py-1 text-sm text-white bg-black rounded-md shadow-lg opacity-90 whitespace-nowrap">
+//         {info}
+//       </span> */}
+//     </div>
+//   );
+// };
+
+// export default DeviceCard;
+
+
 import React from 'react';
 
-const DeviceCard = ({ title, description, info }) => {
+const DeviceCard = ({ title, description, info, image }) => {
   return (
     <div className="relative group">
-      <div className="bg-[#202124] text-white w-54 h-20 p-2 m-2 rounded border border-[#26947a] cursor-pointer">
+      <div className="bg-[#202124] text-white w-54 h-30 p-2 m-2 rounded border border-[#26947a] cursor-pointer flex flex-col items-center">
         <h3 className="text-lg font-bold">{title}</h3>
+        <img src={image} alt= "info" className="h-15 w-auto text-white fill-current" />
         <p className="text-xs mt-2 truncate group-hover:whitespace-normal group-hover:text-xs">
           {description}
         </p>
--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -19,18 +19,75 @@
 
 // export default Sidebar;
 
+// import React from 'react';
+// import DeviceCard from './DeviceCard';
+// import devices from '../data/devices.json';
+// import pm from '../assets/power_meter.svg';
+
+// const Sidebar = ({ visible }) => {
+//   return (
+//     <aside className={`sidebar bg-gray-[#1E1E1E] text-white w-64 sm:w-62 md:w-60 px-4 py-2 border-r border-[#D9D9D9] max-h-screen ${visible ? 'block' : 'hidden md:block'}`}>
+//       <h5 className="text-md font-bold mt-10 sticky top-0 text-[#26947a]">AVAILABLE DEVICES</h5>
+//       <div className="flex justify-center overflow-y-auto h-full pb-4 custom-scrollbar card-container">
+//         <div className="grid grid-cols-1 gap-2 mt-4">
+//           {devices.map((device, index) => (
+//             <DeviceCard key={index} title={device.title} description={device.description} info={device.info} image={pm}/>
+//           ))}
+//         </div>
+//       </div>
+//     </aside>
+//   );
+// };
+
+// export default Sidebar;
+
+
+
 import React from 'react';
 import DeviceCard from './DeviceCard';
 import devices from '../data/devices.json';
+import power_meter from '../assets/power_meter.svg';
+import logo from '../assets/logo.svg';
+import camera from '../assets/camera.svg';
+import spectrometer from '../assets/spectrometer.svg';
+import cnc from '../assets/cnc.svg';
+import controller from '../assets/controller.svg';
+import electronics_board from '../assets/electronics_board.svg';
+import laser from '../assets/laser.svg';
+import equipment from '../assets/equipment.svg';
+import polarization from '../assets/polarization.svg';
+import sensor from '../assets/sensor.svg';
+import stages from '../assets/stages.svg';
+import temperature_controller from '../assets/temperature_controller.svg';
+import amplifier from '../assets/amplifier.svg';
 
 const Sidebar = ({ visible }) => {
+
+const imageMap = {
+    'power_meter.svg': power_meter,
+    'logo.svg': logo,
+    'camera.svg': camera,
+    'spectrometer.svg': spectrometer,
+    'cnc.svg': cnc,
+    'controller.svg': controller,
+    'electronics_board.svg': electronics_board,
+    'laser.svg': laser,
+    'equipment.svg': equipment,
+    'polarization.svg': polarization,
+    'sensor.svg': sensor,
+    'stages.svg': stages,
+    'temperature_controller.svg': temperature_controller,
+    'amplifier.svg': amplifier,
+    };
+
+
   return (
     <aside className={`sidebar bg-gray-[#1E1E1E] text-white w-64 sm:w-62 md:w-60 px-4 py-2 border-r border-[#D9D9D9] max-h-screen ${visible ? 'block' : 'hidden md:block'}`}>
       <h5 className="text-md font-bold mt-10 sticky top-0 text-[#26947a]">AVAILABLE DEVICES</h5>
       <div className="flex justify-center overflow-y-auto h-full pb-4 custom-scrollbar card-container">
         <div className="grid grid-cols-1 gap-2 mt-4">
           {devices.map((device, index) => (
-            <DeviceCard key={index} title={device.title} description={device.description} info={device.info} />
+            <DeviceCard key={index} title={device.title} description={device.description} info={device.info} image={imageMap[device.image]}/>
           ))}
         </div>
       </div>
@@ -39,6 +96,11 @@ const Sidebar = ({ visible }) => {
 };
 
 export default Sidebar;
+
+
+
+
+
 
 
 
